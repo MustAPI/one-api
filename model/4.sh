2148cc433428b07363e123cc1abdb83742a72457
MuskPM <160765632+MustAPI@users.noreply.github.com>
2024-04-11 11:56:18 +0800

    Update 4.sh

--- model/4.sh
+++ model/4.sh
@@ -208,20 +208,20 @@ default_config_json=$(cat <<EOF
 {
   "AustraliaEast": [
     {"deployName": "gpt-35-turbo", "modelName": "gpt-35-turbo", "version": "0613", "capacity": 240},
-    {"deployName": "gpt-4-1106-preview", "modelName": "gpt-4", "version": "1106-Preview", "capacity": 80},
-    {"deployName": "gpt-4", "modelName": "gpt-4", "version": "0613", "capacity": 40},
+    {"deployName": "gpt-4", "modelName": "gpt-4", "version": "1106-Preview", "capacity": 80},
+    {"deployName": "gpt-4-1106-preview", "modelName": "gpt-4", "version": "0613", "capacity": 40},
     {"deployName": "gpt-4-32k", "modelName": "gpt-4-32k", "version": "0613", "capacity": 80},
     {"deployName": "gpt-4-vision-preview", "modelName": "gpt-4", "version": "vision-preview", "capacity": 30}
   ],
   "CanadaEast": [
     {"deployName": "gpt-35-turbo", "modelName": "gpt-35-turbo", "version": "0613", "capacity": 240},
-    {"deployName": "gpt-4-1106-preview", "modelName": "gpt-4", "version": "1106-Preview", "capacity": 80},
-    {"deployName": "gpt-4", "modelName": "gpt-4", "version": "0613", "capacity": 40},
+    {"deployName": "gpt-4", "modelName": "gpt-4", "version": "1106-Preview", "capacity": 80},
+    {"deployName": "gpt-4-1106-preview", "modelName": "gpt-4", "version": "0613", "capacity": 40},
     {"deployName": "gpt-4-32k", "modelName": "gpt-4-32k", "version": "0613", "capacity": 80}
   ],
   "SwitzerlandNorth": [
     {"deployName": "gpt-35-turbo", "modelName": "gpt-35-turbo", "version": "0613", "capacity": 240},
-    {"deployName": "gpt-4", "modelName": "gpt-4", "version": "0613", "capacity": 40},
+    {"deployName": "gpt-4-1106-preview", "modelName": "gpt-4", "version": "0613", "capacity": 40},
     {"deployName": "gpt-4-32k", "modelName": "gpt-4-32k", "version": "0613", "capacity": 80},
     {"deployName": "gpt-4-vision-preview", "modelName": "gpt-4", "version": "vision-preview", "capacity": 30}
   ],
@@ -244,6 +244,18 @@ default_config_json=$(cat <<EOF
   ],
   "NORWAYEAST": [
     {"deployName": "gpt-4-1106-preview", "modelName": "gpt-4", "version": "1106-Preview", "capacity": 150}
+  ],
+  "NorthCentralUS": [
+    {"deployName": "gpt-35-turbo", "modelName": "gpt-35-turbo", "version": "0613", "capacity": 240},
+    {"deployName": "gpt-4-1106-preview", "modelName": "gpt-4", "version": "0125-Preview", "capacity": 80}
+  ],
+   "EastUS": [
+    {"deployName": "gpt-35-turbo", "modelName": "gpt-35-turbo", "version": "0613", "capacity": 240},
+    {"deployName": "gpt-4-1106-preview", "modelName": "gpt-4", "version": "0125-Preview", "capacity": 80}
+  ],
+  "SouthCentralUS": [
+    {"deployName": "gpt-35-turbo", "modelName": "gpt-35-turbo", "version": "0125", "capacity": 240},
+    {"deployName": "gpt-4-1106-preview", "modelName": "gpt-4", "version": "0125-Preview", "capacity": 80}
   ],
    "WestUS": [
     {"deployName": "gpt-35-turbo", "modelName": "gpt-35-turbo", "version": "1106", "capacity": 120},
