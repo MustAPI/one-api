33cc04c7ab7ab3b88d109c95a0eddb636bb047eb
MuskPM <160765632+MustAPI@users.noreply.github.com>
2024-05-16 23:39:28 +0800

    Update 6.sh

--- model/6.sh
+++ model/6.sh
@@ -238,7 +238,8 @@ default_config_json=$(cat <<EOF
   "AustraliaEast": [
     {"deployName": "gpt-35-turbo", "modelName": "gpt-35-turbo", "version": "0613", "capacity": 300},
     {"deployName": "gpt-4-1106-preview", "modelName": "gpt-4", "version": "1106-Preview", "capacity": 80},
-    {"deployName": "gpt-4", "modelName": "gpt-4", "version": "0613", "capacity": 40}
+    {"deployName": "gpt-4", "modelName": "gpt-4", "version": "0613", "capacity": 40},
+    {"deployName": "gpt-4-vision-preview", "modelName": "gpt-4", "version": "vision-preview", "capacity": 80}
   ],
   "CanadaEast": [
     {"deployName": "gpt-35-turbo", "modelName": "gpt-35-turbo", "version": "0613", "capacity": 300},
@@ -248,13 +249,15 @@ default_config_json=$(cat <<EOF
   "SwitzerlandNorth": [
     {"deployName": "gpt-35-turbo", "modelName": "gpt-35-turbo", "version": "0613", "capacity": 300},
     {"deployName": "gpt-4", "modelName": "gpt-4", "version": "0613", "capacity": 40},
-    {"deployName": "gpt-4-32k", "modelName": "gpt-4-32k", "version": "0613", "capacity": 80}
+    {"deployName": "gpt-4-32k", "modelName": "gpt-4-32k", "version": "0613", "capacity": 80},
+    {"deployName": "gpt-4-vision-preview", "modelName": "gpt-4", "version": "vision-preview", "capacity": 80}
   ],
   "SwedenCentral": [
     {"deployName": "gpt-35-turbo", "modelName": "gpt-35-turbo", "version": "0613", "capacity": 300},
     {"deployName": "gpt-4", "modelName": "gpt-4", "version": "0613", "capacity": 40},
     {"deployName": "gpt-4-turbo-2024-04-09", "modelName": "gpt-4", "version": "turbo-2024-04-09", "capacity": 150},
-    {"deployName": "gpt-4-32k", "modelName": "gpt-4-32k", "version": "0613", "capacity": 80}
+    {"deployName": "gpt-4-32k", "modelName": "gpt-4-32k", "version": "0613", "capacity": 80},
+    {"deployName": "gpt-4-vision-preview", "modelName": "gpt-4", "version": "vision-preview", "capacity": 80}
   ],
    "FranceCentral": [
     {"deployName": "gpt-35-turbo", "modelName": "gpt-35-turbo", "version": "0613", "capacity": 240},
